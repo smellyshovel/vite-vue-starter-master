4da0c850f3fdd5a97655154e39d4e1f68d144657
Mattew Mamonov <g.smellyshovel@gmail.com>
2022-10-24 20:41:06 +0400

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,38 @@
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { visualizer } from "rollup-plugin-visualizer";
+
+// https://vitejs.dev/config/
+export default ({ mode }) => {
+  // make all the Vite's env. variables available in this config file
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [vue(), visualizer()],
+
+    resolve: {
+      // add here all the slaves' peer dependencies
+      dedupe: ["vue"],
+    },
+
+    optimizeDeps: {
+      // add here all the slaves
+      exclude: ["vite-vue-starter-slave"],
+    },
+
+    build: {
+      sourcemap: true,
+    },
+
+    server: {
+      // allow the access to all the slaves' sourcemap files
+      fs: {
+        allow: [".."],
+      },
+
+      proxy: {
+        // dev server proxy rules
+      },
+    },
+  });
+};
